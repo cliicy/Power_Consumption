10fcec1aede251310cceb8be97891e619657585c
cliicy <cliicy.luo@scaleflux.com>
2021-01-20 14:32:47 +0800

    kill iostat and sfx-status after every fio

--- dopower_oem.sh
+++ dopower_oem.sh
@@ -23,21 +23,30 @@ if [ ! -d "${timestamp}/randw" ]; then mkdir ${timestamp}/randw; fi
 if [ ! -d "${timestamp}/randmix_rw" ]; then mkdir ${timestamp}/randmix_rw; fi
 
 #precondition:128K seq write 1 loops
-iostat -dxmct 1 ${disk} > ${timestamp}/pre/${disk}.iostat &
+iostat -dxmct 5 ${disk} > ${timestamp}/pre/${disk}.iostat &
 sh powerstatus.sh ${timestamp}/pre ${disk} &
 sudo fio --ioengine=libaio --direct=1 --thread --norandommap --filename=/dev/${disk} --name=init_seq --output=${timestamp}/pre/init_seq.log --rw=write --bs=128k --numjobs=1 --iodepth=128 ${comp_opt_str} --loops=1 
+pkill -9 iostat
+sudo pkill -9 sfx-status
 
 ##大压力顺序写IO，运行脚本如下：
-iostat -dxmct 1 ${disk} > ${timestamp}/seqw/${disk}.iostat &
+iostat -dxmct 5 ${disk} > ${timestamp}/seqw/${disk}.iostat &
 sh powerstatus.sh ${timestamp}/seqw ${disk} &
 sudo fio --name=seq_write --filename=/dev/${disk} --ioengine=libaio --direct=1 --thread=1 --numjobs=1 --iodepth=128 --rw=write --bs=128k --runtime=1h --time_based=1 --size=100% --group_reporting --write_bw_log=${timestamp}/seqw/128K_seqW_bw.log ${comp_opt_str} --output=${timestamp}/seqw/fio-128K_seqW.out
+pkill -9 iostat
+sudo pkill -9 sfx-status
 
 ##大压力随机写IO，运行脚本如下：
-iostat -dxmct 1 ${disk} > ${timestamp}/randw/${disk}.iostat &
+iostat -dxmct 5 ${disk} > ${timestamp}/randw/${disk}.iostat &
 sh powerstatus.sh ${timestamp}/randw ${disk} &
 sudo fio --name=random_write --filename=/dev/${disk} --ioengine=libaio --direct=1 --thread=1 --numjobs=1 --iodepth=128 --rw=randwrite --bs=4k --runtime=1h --time_based=1 --size=100% --norandommap=1 --randrepeat=0 --group_reporting --write_bw_log=${timestamp}/randw/128K_randW_bw.log ${comp_opt_str} --output=${timestamp}/randw/fio-128K_randW.out
+pkill -9 iostat
+sudo pkill -9 sfx-status
 
 ##大压力混合随机写IO，运行脚本如下：
-iostat -dxmct 1 ${disk} > ${timestamp}/randmix_rw/${disk}.iostat &
+iostat -dxmct 5 ${disk} > ${timestamp}/randmix_rw/${disk}.iostat &
 sh powerstatus.sh ${timestamp}/randmix_rw ${disk} &
 sudo fio --name=random_mix --filename=/dev/${disk} --ioengine=libaio --direct=1 --thread=1 --numjobs=8 --iodepth=128 --rw=randrw --rwmixread=50 --bs=4k --runtime=1h --time_based=1 --size=100% --norandommap=1 --randrepeat=0 --group_reporting --write_bw_log=${timestamp}/randmix_rw/128K_mixrw_bw.log ${comp_opt_str} --output=${timestamp}/randmix_rw/fio-128K_mixrw.out
+
+pkill -9 iostat
+sudo pkill -9 sfx-status
